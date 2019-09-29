11428f33f6bc19a23892411111d8552b90eb4f09
Pat P <sell445@gmail.com>
2019-09-29 19:37:44 -0400

    Added put route

--- app.js
+++ app.js
@@ -46,6 +46,24 @@ app.post('/quotes', async (req, res) => {
 
 });
 // Send a PUT request to /quotes/:id to UPDATE(edit) a quotes
+app.put('/quotes/:id', async (req,res) => {
+  try {
+    const quote = await records.getQuote(req.params.id);
+    if (quote) {
+      quote.quote = req.body.quote;
+      quote.author = req.body.author;
+
+      await records.updateQuote(quote);
+      res.status(204).end();
+    } else {
+      res.status(404).json({message: "Quote not found"})
+    }
+    records.updateQuote(quote)
+  } catch (err) {
+    res.status(500).json({message: err.message});
+  }
+})
+
 // Send a DELETE request to /quotes/:id to DELETE a quote
 // Send a GET request to /quotes/quote/random to READ a random quote
 
